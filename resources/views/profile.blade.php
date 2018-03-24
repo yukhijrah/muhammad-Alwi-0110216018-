92e55e497cbfbace836727a0202de82931673836
mAlwiMhfd <muhammadalwi27@gmail.com>
2018-03-25 01:55:11 +0700

    Menambahkan Profile dan CV

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,15 +1,126 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Page Title</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-  <script src="main.js"></script>
-</head>
-<body>
-  <div class=""
-  
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>Sariasuryan</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/bootstrap-4.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="/css/carousel.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Carousel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="/profile">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+
+    <main role="main">
+    <div class="container">
+    <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+    </div>
+    </div>
+    </div>
+
+</br></br>
+<div class="container">
+      <div class="row">
+      <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+      <img class="rounded-circle" src="bootstrap-4.0.0/assets/img/awi.jpg" width="225" height="280">
+      </div>
+        <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title">Muhammad Alwi</h3>
+
+            </div>
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                        <td>Keahlian</td>
+                        <td>:</td>
+                        <td>Mobile apps Developer, Web Designer</td>
+                      </tr>
+                      <tr>
+                        <td>Tanggal Lahir</td>
+                        <td>:</td>
+                        <td>27/12/1998</td>
+                      </tr>
+                      <tr>
+                        <td>Jenis Kelamin</td>
+                        <td>:</td>
+                        <td>Laki-Laki</td>
+                      </tr>
+                        <tr>
+                        <td>Alamat</td>
+                        <td>:</td>
+                        <td>Leuwiliang, Bogor</td>
+                      </tr>
+                      <tr>
+                        <td>Email</td>
+                        <td>:</td>
+                        <td><a href="muhammadalwi27@gmail.com">muhammadalwi27@gmail.com</a></td>
+                      </tr>
+                      <tr>
+                        <td>Telepon</td>
+                        <td>:</td>
+                        <td>0857-1408-2960</td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+              </div>
+
+        </div>
+      </div>
+
+
+        <!-- START THE FEATURETTES -->
+
+      <!-- FOOTER -->
+      <footer class="container">
+        <p>&copy; 2017-2018 Muhammad Alwi &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+    </main>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="/bootstrap-4.0.0/assets/js/vendor/popper.min.js"></script>
+    <script src="/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="/bootstrap-4.0.0/assets/js/vendor/holder.min.js"></script>
+  </body>
 </html>
